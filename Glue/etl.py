b6f92996272d0fdc75558145b6db82871933645d
Dev_Ninja_97 <eliasmoselakgomo@yahoo.com>
2023-07-24 23:23:23 +0200

    etl_solution

--- Glue/etl.py
+++ Glue/etl.py
@@ -1,1 +1,183 @@
+import boto3
+import logging
+from datetime import datetime, timedelta
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession, GlueContext
+from pyspark.sql.window import Window
+from pyspark.sql import functions as F
+import json
+import pandas as pd
+import calendar
+
+# Configure Python logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+s3_base_path = 's3a://banking_data'
+landing = 'bronze'
+transform = 'silver'
+year_month = datetime.now().strftime('%Y%m')
+
+def get_secrets(secret_name):
+    try:
+        secrets_manager_client = boto3.client('secretsmanager', region_name='region')
+        response = secrets_manager_client.get_secret_value(SecretId=secret_name)
+        secret_data = response.get('SecretString')
+        if not secret_data:
+            raise ValueError('Failed to retrieve secrets from AWS Secrets Manager.')
+        return json.loads(secret_data)
+    except Exception as e:
+        logger.error('Error while retrieving secrets from AWS Secrets Manager.')
+        logger.exception(e)
+        raise
+
+def create_spark_session():
+    try:
+        sc = SparkContext()
+        spark = SparkSession(sc).builder.appName('Data Extraction').getOrCreate()
+        return spark
+    except Exception as e:
+        logger.error('Failed to create SparkSession.')
+        logger.exception(e)
+        raise
+
+def extract_monthly_data(spark, aurora_properties, table_name, start_date, end_date):
+    try:
+
+        sql_query = f'''
+            SELECT *
+            FROM {table_name}
+            WHERE date_column >= '{start_date}' AND date_column < '{end_date}'
+        '''
+
+        logger.info('Starting data extraction process for table: %s', table_name)
+
+        with spark.read \
+            .format('jdbc') \
+            .option('driver', aurora_properties['driver']) \
+            .option('url', aurora_properties['url']) \
+            .option('dbtable', f'({sql_query}) as subquery') \
+            .option('user', aurora_properties['user']) \
+            .option('password', aurora_properties['password']) \
+            .load() as df:
+
+            logger.info('Data loaded successfully from Aurora.')
+
+            
+            s3_output_path = f'{s3_base_path}/{landing}/{table_name}/{year_month}/{table_name}_monthly_data_extract.csv'
+            df.write.mode('overwrite').csv(s3_output_path)
+
+            logger.info('Data saved successfully to S3.')
+
+    except Exception as e:
+        logger.error('An error occurred during data extraction for table: %s', table_name)
+        logger.exception(e)
+        raise
+
+    finally:
+        logger.info('Data extraction process completed for table: %s', table_name)
+
+def transform_monthly_data(spark, table_name):
+    try:
+             
+         # Load the tables from S3 into DataFrames
+
+        df1 = spark.read.option('header', 'true').csv(f'{s3_base_path}/{landing}/{table_name}/*.csv')
+        df2 = spark.read.option('header', 'true').csv(f'{s3_base_path}/{landing}/{table_name}/*.csv')
+        df3 = spark.read.option('header', 'true').csv(f'{s3_base_path}/{landing}/{table_name}/*.csv')
+        df4 = spark.read.option('header', 'true').csv(f'{s3_base_path}/{landing}/{table_name}/*.csv')
+        df5 = spark.read.option('header', 'true').csv(f'{s3_base_path}/{landing}/{table_name}/*.csv')
+
+        # Perform the join operation
+
+        joined_df = df1.join(df2, df1['idBank'] == df2['Bank_idBank']) \
+                    .join(df3, df1['idBranch'] == df3['Branch_idBranch']) \
+                    .join(df4, df1['idClient'] == df4['Client_idClient']) \
+                    .join(df5, df1['idAccount'] == df5['Account_idAccount'])
+        
+        
+        joined_df = joined_df.orderBy('date')
+
+        # Convert the date column to DateType
+        joined_df = joined_df.withColumn('loan_date', F.to_date('year_month', 'yyyy-MM'))
+
+
+        # Calculate the moving average of loan amounts taken out over the last three months by branch
+        window_spec = Window.partitionBy('branch').orderBy(F.col('loan_date')).rowsBetween(-2, 0)
+        joined_df = joined_df.withColumn('avgLoanAmount', F.avg('Amount').over(window_spec))
+
+        #final_df = final_df.select(Name,'idBranch','avgLoanAmount')
+
+        current_date = datetime.now().strftime('%Y%m%d')
+        # Group by bank name dimension and save CSV files for each bank
+        bank_names = final_df.select('Name').distinct().rdd.flatMap(lambda x: x).collect()
+        for bank_name in bank_names:
+            final_df = final_df.filter(final_df.bank_name == bank_name)
+            final_df = final_df.select('idBranch','avgLoanAmount')
+            s3_output_path = f'{s3_base_path}/{transform}/{bank_name}/{year_month}/{bank_name}_{current_date}.csv'
+            final_df.write.mode('overwrite').csv(s3_output_path, header=True)
+
+
+
+
+    except Exception as e:
+        logger.error('An unexpected error occurred.')
+        logger.exception(e)
+
+
+def main():
+    try:
+        glueContext = GlueContext(SparkContext.getOrCreate())
+        args = glueContext._jvm.sys.argv
+        if len(args) < 2:
+            raise ValueError('No table names provided as job parameters.')
+        
+        # Extract the list of table names passed as job parameters
+        table_names = args[1:]
+
+        spark = create_spark_session()
+
+        # Get credentials from AWS Secrets Manager for Aurora
+        aurora_credentials = get_secrets('your-aurora-secret-name')
+
+        # Get Aurora endpoint (hostname) from the secrets
+        aurora_endpoint = aurora_credentials.get('endpoint')
+        if not aurora_endpoint:
+            raise ValueError('Aurora endpoint not found in Secrets Manager response.')
+
+        # Set the Aurora connection properties
+        aurora_properties = {
+            'user': aurora_credentials.get('username'),
+            'password': aurora_credentials.get('password'),
+            'driver': 'com.mysql.jdbc.Driver',
+            'url': f'jdbc:mysql://{aurora_endpoint}:3306/BankingDB'
+        }
+
+        current_date = datetime.now()
+
+        # Get the first and last day of the current month
+        _, last_day = calendar.monthrange(current_date.year, current_date.month)
+        start_date = current_date.replace(day=1).date()
+        end_date = current_date.replace(day=last_day).date()
+
+    
+        # Stage the data
+        for table_name in table_names:
+            extract_monthly_data(spark, aurora_properties, table_name, start_date, end_date)
+
+        #Load Transformed Data
+        transform_monthly_data()
+
+        spark.stop()
+
+    except Exception as e:
+        logger.error('An unexpected error occurred.')
+        logger.exception(e)
+
+if __name__ == '__main__':
+    main()
 
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,304 @@
-##################
-# Glue Catalog   #
-##################
+# Configure the AWS provider
+provider "aws" {
+  region = var.region
+}
 
-##################
-# Glue Crawler   #
-##################
 
-##################
-# Glue Job       #
-##################
+# Create an Amazon SNS topic for notifications
+resource "aws_sns_topic" "job_failure_notification" {
+  name = "JobFailureNotificationTopic"
+}
+
+# Subscribe the support email to the SNS topic
+resource "aws_sns_topic_subscription" "job_failure_notification_subscription" {
+  topic_arn = aws_sns_topic.job_failure_notification.arn
+  protocol  = "email"
+  endpoint  = "data-support@mybigbank.co.za"
+}
+
+# Create an IAM role for the Step Functions state machine
+resource "aws_iam_role" "step_functions_role" {
+  name        = "step_functions_role"
+  description = "Role for Step Functions state machine"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "states.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the required policies to the Step Functions role
+resource "aws_iam_role_policy_attachment" "step_functions_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSStepFunctionsFullAccess"
+  role       = aws_iam_role.step_functions_role.name
+}
+
+# Create an IAM policy for publishing SNS messages for the Step Functions role
+resource "aws_iam_policy" "sns_publish_policy" {
+  name        = "StepFunctionsSNSPublishPolicy"
+  description = "IAM policy to allow publishing SNS messages for Step Functions"
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "sns:Publish",
+        "Resource": aws_sns_topic.job_failure_notification.arn
+      }
+    ]
+  })
+}
+
+# Attach the SNS publish policy to the Step Functions role
+resource "aws_iam_role_policy_attachment" "sns_publish_policy_attachment" {
+  policy_arn = aws_iam_policy.sns_publish_policy.arn
+  role       = aws_iam_role.step_functions_role.name
+}
+
+# Create the Step Functions state machine definition
+data "aws_s3_bucket_object" "state_machine_definition" {
+  bucket = aws_s3_bucket.glue_scripts_bucket.bucket
+  key    = "state_machine_definition.json" # Replace with the path to your state machine definition JSON file
+}
+
+# Create the Step Functions state machine
+resource "aws_sfn_state_machine" "glue_job_scheduler" {
+  name     = "GlueJobScheduler"
+  role_arn = aws_iam_role.step_functions_role.arn
+  definition = data.aws_s3_bucket_object.state_machine_definition.body # Replace with the state machine definition JSON content
+}
+
+# Create a CloudWatch Events rule to schedule the Step Functions state machine
+resource "aws_cloudwatch_event_rule" "glue_job_schedule" {
+  name        = "GlueJobSchedule"
+  description = "Schedule for Glue Job State Machine"
+
+  schedule_expression = "rate(1 day)" # Change this expression to your desired schedule
+}
+
+# Add the Step Functions state machine as a target for the CloudWatch Events rule
+resource "aws_cloudwatch_event_target" "glue_job_schedule_target" {
+  rule      = aws_cloudwatch_event_rule.glue_job_schedule.name
+  target_id = "GlueJobScheduleTarget"
+  arn       = aws_sfn_state_machine.glue_job_scheduler.arn
+}
+
+# Trigger the Glue job from the Step Functions state machine
+resource "aws_sfn_activity" "trigger_glue_job_activity" {
+  name = "TriggerGlueJobActivity"
+}
+
+# Define the state machine definition JSON content (replace with your actual Glue job ARN)
+locals {
+  glue_job_arn = aws_glue_job.monthly_etl_job.arn
+}
+
+data "aws_sfn_state_machine" "state_machine" {
+  name = aws_sfn_state_machine.glue_job_scheduler.name
+}
+
+resource "aws_sfn_state_machine" "state_machine_definition" {
+  name     = aws_sfn_state_machine.glue_job_scheduler.name
+  role_arn = aws_iam_role.step_functions_role.arn
+  definition = jsonencode({
+    Comment = "Glue Job Scheduler",
+    StartAt = "ScheduleGlueJob",
+    States = {
+      ScheduleGlueJob = {
+        Type     = "Task",
+        Resource = aws_sfn_activity.trigger_glue_job_activity.arn,
+        TimeoutSeconds = 60,
+        End = true
+      },
+      JobFailedNotification = {
+        Type = "Fail",
+        Error = "JobFailed",
+        Cause = "The Glue job has failed. Please check the AWS Glue console for more details.",
+        End = true
+      }
+    }
+  })
+}
+
+resource "aws_sfn_activity" "trigger_glue_job_activity" {
+  name = "TriggerGlueJobActivity"
+}
+
+# Associate the Lambda function as a target with the CloudWatch Event rule
+resource "aws_cloudwatch_event_target" "rotation_trigger" {
+  rule      = aws_cloudwatch_event_rule.monthly_rotation_schedule.name
+  target_id = "rotation_trigger"
+  arn       = aws_lambda_function.secrets_rotation_lambda.arn
+}
+
+# Create a Glue job
+resource "aws_glue_job" "monthly_etl_job" {
+  name         = "monthly_etl_job"
+  description  = "Monthly data extraction job"
+  role_arn     = aws_iam_role.glue_job_role.arn
+  command {
+    script_location = "s3://${aws_s3_bucket.glue_scripts_bucket.id}/etl.py"
+    python_version  = "3"
+  }
+
+  timeout     = 60
+  max_retries = 2
+}
+
+# Configure the AWS provider
+provider "aws" {
+  region = var.region
+}
+
+# Create an S3 bucket for Glue scripts
+resource "aws_s3_bucket" "glue_scripts_bucket" {
+  bucket = "glue-scripts-bucket" # Change this to a unique bucket name
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
+
+# Create an S3 bucket for files
+resource "aws_s3_bucket" "files_bucket" {
+  bucket = "banking_data" # Change this to a unique bucket name
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# Create a Glue catalog database
+resource "aws_glue_catalog_database" "my_database" {
+  name     = "my_database"
+  location = "s3://${aws_s3_bucket.glue_scripts_bucket.id}"
+  start_date = var.start_date
+  end_date   = var.end_date
+}
+
+
+
+# Create an AWS Secrets Manager secret for Glue job credentials
+resource "aws_secretsmanager_secret" "glue_job_credentials" {
+  name        = "glue_job_credentials"
+  description = "Credentials for Glue job"
+}
+
+# Create a new version of the secret with the necessary credentials
+resource "aws_secretsmanager_secret_version" "glue_job_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.glue_job_credentials.id
+  secret_string = jsonencode({
+    aws_access_key_id     = var.aws_access_key_id,
+    aws_secret_access_key = var.aws_secret_access_key,
+    aurora_username       = var.aurora_username,
+    aurora_password       = var.aurora_password
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create an AWS Lambda function for secrets rotation
+resource "aws_lambda_function" "secrets_rotation_lambda" {
+  filename      = "secrets_rotation_lambda.zip"
+  function_name = "secrets_rotation_lambda"
+  role          = aws_iam_role.secrets_rotation_lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  timeout       = 300
+
+  environment {
+    variables = {
+      secret_id = aws_secretsmanager_secret.glue_job_credentials.id
+    }
+  }
+}
+
+# Create an IAM role for the Lambda function
+resource "aws_iam_role" "secrets_rotation_lambda_role" {
+  name        = "secrets_rotation_lambda_role"
+  description = "Role for Secrets Rotation Lambda"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the AWSLambdaBasicExecutionRole policy to the Lambda function role
+resource "aws_iam_role_policy_attachment" "secrets_rotation_lambda_policy_attachment" {
+  role       = aws_iam_role.secrets_rotation_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Create a CloudWatch Event rule for monthly rotation schedule
+resource "aws_cloudwatch_event_rule" "monthly_rotation_schedule" {
+  name        = "monthly_rotation_schedule"
+  description = "Monthly rotation schedule"
+
+  schedule_expression = "rate(1 month)"
+}
+
+# Associate the Lambda function as a target with the CloudWatch Event rule
+resource "aws_cloudwatch_event_target" "rotation_trigger" {
+  rule      = aws_cloudwatch_event_rule.monthly_rotation_schedule.name
+  target_id = "rotation_trigger"
+  arn       = aws_lambda_function.secrets_rotation_lambda.arn
+}
+
+
+
+# Create an IAM role for the Glue job
+resource "aws_iam_role" "glue_job_role" {
+  name        = "glue_job_role"
+  description = "Role for Glue job"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "glue.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the AWSGlueServiceRole policy to the Glue job role
+resource "aws_iam_role_policy_attachment" "glue_job_policy_attachment" {
+  role       = aws_iam_role.glue_job_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+
 
